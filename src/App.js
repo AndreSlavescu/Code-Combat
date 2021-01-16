e096d7a144835eb6406e0ad3c4b028af0a430444
KnlnKS <soulkks@hotmail.ca>
2021-01-16 01:42:29 -0500

    Fix Firebase Errors + Add Conditional Rendering

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-import firebase from './firebaseConfig';
+import firebase from "./firebaseConfig";
 import "firebase/firestore";
 import "firebase/auth";
 
@@ -10,6 +10,7 @@ import { useCollectionData } from "react-firebase-hooks/firestore";
 
 // Components - Pages
 import Landing from "./components/Landing";
+import Login from "./components/Login";
 
 // Components - Utils
 import SignIn from "./components/utils/SignIn";
@@ -24,24 +25,44 @@ function App() {
   return (
     <Router className="App">
       <nav>
-        <ul>
-        <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/competitive">Competitive</Link>
-          </li>
-          <li>
-            <Link to="/unranked">Unranked</Link>
-          </li>
-          <li>
-            <Link to="/custom">Custom</Link>
-          </li>
-        </ul>
+        {user ? (
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/competitive">Competitive</Link>
+            </li>
+            <li>
+              <Link to="/unranked">Unranked</Link>
+            </li>
+            <li>
+              <Link to="/custom">Custom</Link>
+            </li>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+            <li>
+              <SignOut />
+            </li>
+          </ul>
+        ) : (
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <SignIn />
+            </li>
+          </ul>
+        )}
       </nav>
 
       <Switch>
-
+        <Route path="/competitive">{user ? <Landing /> : <Login />}</Route>
+        <Route path="/unranked">{user ? <Landing /> : <Login />}</Route>
+        <Route path="/custom">{user ? <Landing /> : <Login />}</Route>
+        <Route path="/profile">{user ? <Landing /> : <Login />}</Route>
         <Route path="/">
           <Landing />
         </Route>
--- src/components/Utils/SignOut.js
+++ src/components/Utils/SignOut.js
@@ -1,16 +1,7 @@
 import React from "react";
-import firebase from "firebase/app";
+import firebase from "../../firebaseConfig";
 import "firebase/firestore";
 import "firebase/auth";
-
-firebase.initializeApp({
-    apiKey: "AIzaSyDL3o0LcAnj50FZBddw5AQCI0b_UhhIMfA",
-    authDomain: "htn2020-coding-game.firebaseapp.com",
-    projectId: "htn2020-coding-game",
-    storageBucket: "htn2020-coding-game.appspot.com",
-    messagingSenderId: "610958625433",
-    appId: "1:610958625433:web:5cf7c1be66b44bda868608",
-  });
 
 const auth = firebase.auth();
 
@@ -20,4 +11,4 @@ function SignOut() {
     )
   }
 
-export default SignIn;
+export default SignOut;
