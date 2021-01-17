3c748eae4f3b2b8480902000140ceebe6cb34d15
KnlnKS <soulkks@hotmail.ca>
2021-01-16 23:56:12 -0500

    Commdihjf

--- src/components/Game.js
+++ src/components/Game.js
@@ -6,61 +6,29 @@ const auth = firebase.auth();
 const firestore = firebase.firestore();
 
 function Game() {
-  const [widgetHTML, setWidgetHTML] = useState("");
-  const [players, setPlayers] = useState(0);
-  let { id } = useParams();
-
-  const updatePlayerCount = async () => {
-    const game = firestore.collection("games").doc(id);
-    const increment = firebase.firestore.FieldValue.increment(1);
-    game.update({
-      players: increment,
-    });
-  };
-
-  const getGame = () => {
-    return new Promise((resolve, reject) => {
-      firestore.collection("games").onSnapshot((snapshot) => {
-        console.log("onSnapshot Called!");
-        let updatedData = snapshot.docs.map((doc) => {
-          let data = doc.data()[0];
-          return doc.data();
-        });
-        resolve(updatedData);
-      }, reject);
-    });
-  };
-
+  const [widget, setWidget] = useState("");
   useEffect(() => {
-    getGame().then((doc) => {
-      setPlayers(doc[0].players);
-      setWidgetHTML(doc[0].widgetHTML);
-      if (doc[0].players >= 0 && doc[0].players < 2) {
-        updatePlayerCount();
-      }
-    });
+    setWidget(
+      "https://hourofpython.trinket.io/python-challenges#/string-challenges/lowercase-challenge"
+    );
   });
 
   return (
-    <div>
-      <div>{players > 1 ? <Widget HTML={widgetHTML} /> : <div />}</div>
+    <div class="code-content">
+      <div class="content-blocker">
+        <iframe
+          src={widget}
+          id="myframe"
+          scrolling="yes"
+          frameborder="1"
+          marginheight="0px"
+          marginwidth="0px"
+          height="900px"
+          width="700px"
+        ></iframe>
+      </div>
     </div>
   );
-}
-
-function Widget({ HTML }) {
-
-  return (
-    <iframe
-      id="ifrm"
-      src={"https://www.skillstack.com/embedchallenges/?" + HTML}
-      width="100%"
-      height="600"
-    ></iframe>
-  );
-
-
-  return <div>{HTML}</div>;
 }
 
 export default Game;
