a7b3845f25761c757b9d4b144ece7e50208d1777
yamaan93 <yamaan93@gmail.com>
2021-01-17 02:11:29 -0500

    reeee

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -6,7 +6,8 @@ function Profile() {
       <div class="pfp">
         <img src="https://i.imgur.com/pllt0jH.png" />
         <h4>Name: Player1</h4>
-        <h4>ELO: 1027</h4>
+        <img src="https://img.rankedboost.com/wp-content/uploads/2020/04/Valorant-Rank-1.png" class="medal" />
+        <h4>Rank: Gold</h4>
       </div>
     </div>
   );
